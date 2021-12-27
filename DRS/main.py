0bffe6771e2b0f852c133b8722c006c11b052a85
Djrodje Rodic <73758461+rodic99@users.noreply.github.com>
2021-12-27 22:19:57 +0100

    register/login + baza

--- DRS/main.py
+++ DRS/main.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template, flash, request
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+
+import dbconnection
 from dbconnection import connection
 
 
@@ -15,55 +17,38 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template('dashboard.html')
-
+@app.route("/login_redirect", methods=["GET"])
+def login_redirect():
+    return render_template('login.html')
 
 
 
-"""
-@app.route('/login', methods = ["GET", "POST"])
-def login_page():
-    error = None
-    try:
-        if request.method == "POST":
-            attempted_username = request.form["username"]
-            attempted_password = request.form["password"]
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['psw']
 
-
-
-            flash(attempted_username)
-            flash(attempted_password)
-
-    except Exception as e:
-        flash(e)
-        return render_template("login.html", error = error)
-"""
-
-
-class RegistrationForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=50)])
-    lastname = StringField('Last Name', [validators.Length(min=1, max=50)])
-    email = StringField('Email', [validators.Length(min=6, max=50)])
-    address = StringField('Address', [validators.Length(min=6, max=50)])
-    city = StringField('City', [validators.Length(min=2, max=50)])
-    password = PasswordField('Password', validators.DataRequired(), validators.length(min=6, max=15))
-
-
+        conn = connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Users WHERE email= %s AND passwrd= %s", (email, password))
+        if (cursor.fetchone()):
+            return render_template('proba.html', result=email)
+        else:
+            flash("error, wrong password")
 
 @app.route('/register', methods=["GET", "POST"])
 def register_page():
     if request.method == 'POST':
         result = request.form
-        
-
-
+        conn = dbconnection.connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO Users(firstname, lastname, email, address, city, country, passwrd, phoneNumber) "
+                       "VALUES(%s, %s, %s, %s, %s, %s, %s, %s)", (result['name'], result['lastName'], result['email'], result['address'], result['city'], result['country'], result['psw'], result['phone']))
+        conn.commit()
+        cursor.close()
+        conn.close()
     return render_template("proba.html", result=result)
-
-
-
 
 if __name__ == "__main__":
     app.run(port=5000)
